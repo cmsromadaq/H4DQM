7d2e69ec42b1b313e3d0d2b7cc39f71036d93b25
michelif <micheli@pb-d-128-141-105-13.cern.ch>
2014-09-30 13:08:46 +0200

    committing all classes

--- interface/SpillUnpack.hpp
+++ interface/SpillUnpack.hpp
@@ -1,3 +1,33 @@
-#include "EventBuilder.hpp"
-#include "DataType.hpp"
-#include "StandardIncludes.hpp"
+#ifndef SPILLUNPCK_H
+#define SPILLUNPCK_H
+
+#include "interface/EventBuilder.hpp"
+#include "interface/DataType.hpp"
+#include "interface/StandardIncludes.hpp"
+using namespace std;
+
+
+class SpillUnpack{
+
+public:
+
+  SpillUnpack();
+  ~SpillUnpack();
+
+  dataType stream;
+
+
+  EventForTree evtTree_;
+  TFile * outFile_;
+  TTree* outTree_;
+  UInt_t  boardId_;//probably useless
+  UInt_t  boardType_;
+  UInt_t  crateId_;
+  Int_t  nBoardTypes_;
+  map<nBoardTypes_,Board *> boards_;
+  void InitBoards();//create a map between boardType and board
+  int Unpack(dataType &stream);
+  void CreateTree();
+  void inline FillTree(){outTree_->Fill();};
+
+};
