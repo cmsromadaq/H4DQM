b8737d09f167fc5fac5a20a8b8b9d0a51cc4eb8a
Pietro Govoni <govoni@cmsmi4.cern.ch>
2014-10-07 15:12:28 +0200

    some printouts

--- src/CAEN_V792.cpp
+++ src/CAEN_V792.cpp
@@ -1,6 +1,9 @@
 #include "interface/CAEN_V792.hpp"
 #include "interface/Event.hpp"
 
+#define DEBUG_UNPACKER 0
+#define DEBUG_VERBOSE_UNPACKER 0
+
 int CAEN_V792::Unpack (dataType &stream, Event * event) 
 {
 
@@ -9,33 +12,44 @@ int CAEN_V792::Unpack (dataType &stream, Event * event)
   int nWords = 0 ;
 
 //  while (currWord != boardTrailerValue) {
-  for (int i=0;i<adc792Channels_;++i) {
+  for (int i = 0 ; i < adc792Channels_ ; ++i) 
+    {
 
     stream.read ((char*)&currWord, WORDSIZE);
     short dt_type = currWord>>24 & 0x7; //dt_type 0 is datum,2 is BOE,4 is EOE
 
-    if (dt_type==0) {
-      //FOR ADC792 DATA
-      adcData thisData;
-	      
-      //ADC792 -> adcBoard 1
-      thisData.board=1; 
-      thisData.channel = currWord>>16 & 0x1F;
-      thisData.adcReadout = currWord & 0xFFF;
-      if ((currWord>>12) & 0x1) //overflow
-	thisData.adcReadout=4095;
-//      cout << "[CAEN_V792][Unpack]        | Board " << thisData.board
-//           << "  Channel " << thisData.channel
-//           << "  adcReadout " << thisData.adcReadout << endl; 
-      event->adcValues.push_back(thisData);
-    } 
+    if (dt_type==0) 
+      {
+        //FOR ADC792 DATA
+        adcData thisData;
+          
+        //ADC792 -> adcBoard 1
+        thisData.board=1; 
+        thisData.channel = currWord>>16 & 0x1F;
+        thisData.adcReadout = currWord & 0xFFF;
+        if ((currWord>>12) & 0x1) //overflow
+        thisData.adcReadout=4095;
+        if (DEBUG_UNPACKER)
+          {
+            cout << "[CAEN_V792][Unpack]        | Board " << thisData.board
+                 << "  Channel " << thisData.channel
+                 << "  adcReadout " << thisData.adcReadout << endl; 
+          }
+        event->adcValues.push_back(thisData);
+      } 
  
-    if (dt_type==4) {
-      WORD adcEvt=currWord & 0xFFFFFF;
-//      cout << "[CAEN_V792][Unpack]        |  EOE. Event: " << adcEvt+1 << endl; 
-//      if (adcEvt+1 != event->evtNumber)
-//	cout << "[CAEN_V792][Unpack]        | HEY MISMATCH IN EVT NUMBER ADCEVT " << adcEvt+1 << " EVT " << event->evtNumber << endl;
-    }
+    if (dt_type==4) 
+      {
+        WORD adcEvt=currWord & 0xFFFFFF;
+        if (DEBUG_VERBOSE_UNPACKER)
+          {
+            cout << "[CAEN_V792][Unpack]        |  EOE. Event: " << adcEvt+1 << "\n" ; 
+          }
+
+        if (adcEvt+1 != event->evtNumber)
+             cout << "[CAEN_V792][Unpack]        | WARNING MISMATCH IN EVT NUMBER ADCEVT " 
+                  << adcEvt+1 << " EVT " << event->evtNumber << "\n" ;
+      }
 
   }
 }
