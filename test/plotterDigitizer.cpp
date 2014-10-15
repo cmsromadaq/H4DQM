a515b8752bd9ddb6d4813ac1de248ea3344fafaa
Pietro Govoni <govoni@cmsmi4.cern.ch>
2014-10-15 17:18:47 +0200

    adding the required cross-check for folders

--- test/plotterDigitizer.cpp
+++ test/plotterDigitizer.cpp
@@ -15,15 +15,54 @@
 #include <TBranch.h>
 
 #include <set>
+#include <getopt.h>
 
 #include <plotterTools.hpp>
 
 using namespace std ;
 
-// get the histos ranges from a cfg file, written in XML
+
+pair<int, string>
+execute (const string & command) 
+{
+    FILE *in;
+    char buff[512] ;
+    if (!(in = popen (command.c_str (), "r")))
+      {
+        return pair<int, string> (-99, "") ;
+      }
+
+    std::string result, tempo ;
+    while (fgets (buff, sizeof (buff), in)!=NULL)
+      {
+        tempo = buff ;
+        result += tempo ;
+      }
+    int returnCode = pclose (in) ;
+ 
+    return pair<int, string> (returnCode, result) ;
+}
 
 
-// what are the channels which are turned on? i.e. how can I know it?
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+void checkFolder (string folderName)
+{
+  pair <int, string> outCode = execute ("ls " + folderName) ;
+  if (outCode.first != 0) outCode = execute ("mkdir " + folderName) ;
+  if (outCode.first != 0) 
+    {
+      cerr << "[plotDigitizer] ERROR cannot create " << folderName << ":\n"
+           << outCode.second << "\n"
+           << "exiting\n" ;
+      exit (1) ;
+    }
+  return ;
+}
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
 
 void addToPersistency (TH2F * pPlot, TChain * event, 
                        int digiGroup, int digiChannel)
@@ -37,7 +76,6 @@ void addToPersistency (TH2F * pPlot, TChain * event,
   event->Draw (command, selection) ;
   return ;
 }
-
 
 
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -83,40 +121,58 @@ int main (int argc, char ** argv)
   // read the info from command line
   // ---- ---- ---- ---- ---- ---- ---- ----
   
-  char *filename = NULL;
-  char *outfname = NULL;
-  char *outdname = NULL;
-  int c;
+  string PLOTS_FOLDER = "/data2/govoni/data/plots/" ;
+  string DIGI_FOLDER = "/data2/govoni/data/digi/" ;
+  string run;
+  string spill;
 
-  while ((c = getopt (argc, argv, "f:d:o:")) != -1)
-    switch (c)
-      {
-          case 'f':
-            filename = optarg;
-            break;
-         case 'o':
-            outfname = optarg;
-            break;
-          case 'd':
-            outdname = optarg;
-            break;
-          case '?':
-            if (optopt == 'f'|| optopt == 'o'|| optopt == 'd')
-             fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint (optopt))
-              fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-            else
-              fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-            return 1;
-          default:
-            exit (-1);
-      }
+  static struct option long_options[] = {
+    {"plotsfolder", required_argument, 0,  'o' },
+    {"digifolder",  required_argument, 0,  'i' },
+    {"run",         required_argument, 0,  'r' },
+    {"spill",       required_argument, 0,  's' },
+    {0,             0,                 0,  0   }
+  };
+ 
+ int long_index =0;
+ int opt;
+
+ while ((opt = getopt_long(argc, argv,"i:o:r:s:", 
+			   long_options, &long_index )) != -1) {
+   switch (opt) {
+   case 'i' : DIGI_FOLDER=string(optarg);
+     break;
+   case 'o' : PLOTS_FOLDER=string(optarg);
+     break;
+   case 'r' : run=string(optarg);
+     break;
+   case 's' : spill=string(optarg);
+     break;
+   case '?':
+     /* getopt_long already printed an error message. */
+     //print_usage(); 
+     //exit(EXIT_FAILURE);
+   default: 
+     //print_usage(); 
+     exit(1);
+   } // switch
+ } // while
+  
+  cout <<" PLOTS FOLDER IS: "<<PLOTS_FOLDER<<endl;
+  cout <<" DIGI FOLDER IS: "<<DIGI_FOLDER<<endl;
+
   // open the digi file
   // ---- ---- ---- ---- ---- ---- ---- ----
 
-  plotterTools plotter (filename,outfname,outdname) ;
+  string filename = DIGI_FOLDER + "/" + run + "/" + spill + ".root" ;
+  string outdname = PLOTS_FOLDER + "/" + run + "/" + spill + "/digitizer/" ;
+  string outfname = outdname + "plots_" + spill + ".root" ;
+
+  checkFolder (PLOTS_FOLDER + "/" + run) ;
+  checkFolder (PLOTS_FOLDER + "/" + run + "/" + spill) ;
+  checkFolder (PLOTS_FOLDER + "/" + run + "/" + spill + "/digitizer/") ;
+
+  plotterTools plotter (filename.c_str (), outfname.c_str (), outdname.c_str ()) ;
   plotter.setPlotsFormat () ;
   plotter.setModule ("pulses") ;
   plotter.setGroup ("digitizer") ;
