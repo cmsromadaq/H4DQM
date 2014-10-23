a77cbb9126dd1b18a84c47205535bf1ce0d38385
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-23 05:09:27 +0200

    script for hodo small position scanning with digitizer

---
+++ scripts/hodo_scan/hodoScan.C
@@ -0,0 +1,59 @@
+#define hodoScan_cxx
+#include "hodoScan.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <iostream>
+
+void hodoScan::Loop()
+{
+//   In a ROOT session, you can do:
+//      Root > .L hodoScan.C
+//      Root > hodoScan t
+//      Root > t.GetEntry(12); // Fill t data members with entry number 12
+//      Root > t.Show();       // Show values of entry 12
+//      Root > t.Show(16);     // Read and show values of entry 16
+//      Root > t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   float frac[4];
+   for (int i=0; i<4; i++) frac[i]=0;
+
+   const int offset = 4;
+   const float threshold = 25.;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+      for (int i=0; i<4; i++) {
+	if (digi_max_amplitude->at(offset+i)>threshold) frac[i]+=1.0/float(nentries);
+      }
+      // if (Cut(ientry) < 0) continue;
+   }
+
+   std::cout << "RUN " << runNumber << " SPILL " << spillNumber << std::endl; 
+   for (int i=0; i<4; i++) std::cout << frac[i] << " ";
+   std::cout << endl;
+
+}
---
+++ scripts/hodo_scan/hodoScan.h
@@ -0,0 +1,165 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Thu Oct 23 04:33:16 2014 by ROOT version 5.34/20
+// from TTree outputTree/outputTree
+// found on file: dqmPlotstotal.root
+//////////////////////////////////////////////////////////
+
+#ifndef hodoScan_h
+#define hodoScan_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include <vector>
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class hodoScan {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   UInt_t          runNumber;
+   UInt_t          spillNumber;
+   UInt_t          evtNumber;
+   vector<float>   *ADCvalues;
+   vector<float>   *digi_max_amplitude;
+   vector<float>   *digi_pedestal;
+   vector<float>   *digi_pedestal_rms;
+   vector<float>   *digi_time_at_frac30;
+   vector<float>   *digi_time_at_frac50;
+   vector<float>   *digi_time_at_max;
+
+   // List of branches
+   TBranch        *b_runNumber;   //!
+   TBranch        *b_spillNumber;   //!
+   TBranch        *b_evtNumber;   //!
+   TBranch        *b_ADCvalues;   //!
+   TBranch        *b_digi_max_amplitude;   //!
+   TBranch        *b_digi_pedestal;   //!
+   TBranch        *b_digi_pedestal_rms;   //!
+   TBranch        *b_digi_time_at_frac30;   //!
+   TBranch        *b_digi_time_at_frac50;   //!
+   TBranch        *b_digi_time_at_max;   //!
+
+   hodoScan(TTree *tree=0);
+   virtual ~hodoScan();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef hodoScan_cxx
+hodoScan::hodoScan(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("dqmPlotstotal.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("dqmPlotstotal.root");
+      }
+      f->GetObject("outputTree",tree);
+
+   }
+   Init(tree);
+}
+
+hodoScan::~hodoScan()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t hodoScan::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t hodoScan::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void hodoScan::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   ADCvalues = 0;
+   digi_max_amplitude = 0;
+   digi_pedestal = 0;
+   digi_pedestal_rms = 0;
+   digi_time_at_frac30 = 0;
+   digi_time_at_frac50 = 0;
+   digi_time_at_max = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("runNumber", &runNumber, &b_runNumber);
+   fChain->SetBranchAddress("spillNumber", &spillNumber, &b_spillNumber);
+   fChain->SetBranchAddress("evtNumber", &evtNumber, &b_evtNumber);
+   fChain->SetBranchAddress("ADCvalues", &ADCvalues, &b_ADCvalues);
+   fChain->SetBranchAddress("digi_max_amplitude", &digi_max_amplitude, &b_digi_max_amplitude);
+   fChain->SetBranchAddress("digi_pedestal", &digi_pedestal, &b_digi_pedestal);
+   fChain->SetBranchAddress("digi_pedestal_rms", &digi_pedestal_rms, &b_digi_pedestal_rms);
+   fChain->SetBranchAddress("digi_time_at_frac30", &digi_time_at_frac30, &b_digi_time_at_frac30);
+   fChain->SetBranchAddress("digi_time_at_frac50", &digi_time_at_frac50, &b_digi_time_at_frac50);
+   fChain->SetBranchAddress("digi_time_at_max", &digi_time_at_max, &b_digi_time_at_max);
+   Notify();
+}
+
+Bool_t hodoScan::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void hodoScan::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t hodoScan::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef hodoScan_cxx
