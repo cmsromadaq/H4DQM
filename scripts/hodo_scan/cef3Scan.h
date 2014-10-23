75fdd695715ee46642264ac7f7fbda166c750543
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-23 14:45:16 +0200

    first commit

---
+++ scripts/hodo_scan/cef3Scan.h
@@ -0,0 +1,168 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Thu Oct 23 04:33:16 2014 by ROOT version 5.34/20
+// from TTree outputTree/outputTree
+// found on file: dqmPlotstotal.root
+//////////////////////////////////////////////////////////
+
+#ifndef cef3Scan_h
+#define cef3Scan_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1F.h>
+
+// Header file for the classes stored in the TTree if any.
+#include <vector>
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class cef3Scan {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   UInt_t          runNumber;
+   UInt_t          spillNumber;
+   UInt_t          evtNumber;
+   vector<float>   *ADCvalues;
+   vector<float>   *digi_max_amplitude;
+   vector<float>   *digi_charge_integrated;
+   vector<float>   *digi_pedestal;
+   vector<float>   *digi_pedestal_rms;
+   vector<float>   *digi_time_at_frac30;
+   vector<float>   *digi_time_at_frac50;
+   vector<float>   *digi_time_at_max;
+
+   // List of branches
+   TBranch        *b_runNumber;   //!
+   TBranch        *b_spillNumber;   //!
+   TBranch        *b_evtNumber;   //!
+   TBranch        *b_ADCvalues;   //!
+   TBranch        *b_digi_charge_integrated;   //!
+   TBranch        *b_digi_max_amplitude;   //!
+   TBranch        *b_digi_pedestal;   //!
+   TBranch        *b_digi_pedestal_rms;   //!
+   TBranch        *b_digi_time_at_frac30;   //!
+   TBranch        *b_digi_time_at_frac50;   //!
+   TBranch        *b_digi_time_at_max;   //!
+
+   cef3Scan(int runNum);
+   virtual ~cef3Scan();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual TH1F*     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef cef3Scan_cxx
+cef3Scan::cef3Scan(int runNum)
+{
+
+   TFile *f = TFile::Open(Form("/data/public_DQM_plots/%d/dqmPlotstotal.root", runNum) );
+   if (!f || !f->IsOpen()) {
+      f = new TFile("dqmPlotstotal.root");
+   }
+   TTree* tree;
+   f->GetObject("outputTree",tree);
+
+   Init(tree);
+}
+
+cef3Scan::~cef3Scan()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t cef3Scan::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t cef3Scan::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void cef3Scan::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   ADCvalues = 0;
+   digi_charge_integrated = 0;
+   digi_max_amplitude = 0;
+   digi_pedestal = 0;
+   digi_pedestal_rms = 0;
+   digi_time_at_frac30 = 0;
+   digi_time_at_frac50 = 0;
+   digi_time_at_max = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("runNumber", &runNumber, &b_runNumber);
+   fChain->SetBranchAddress("spillNumber", &spillNumber, &b_spillNumber);
+   fChain->SetBranchAddress("evtNumber", &evtNumber, &b_evtNumber);
+   fChain->SetBranchAddress("ADCvalues", &ADCvalues, &b_ADCvalues);
+   fChain->SetBranchAddress("digi_charge_integrated", &digi_charge_integrated, &b_digi_charge_integrated);
+   fChain->SetBranchAddress("digi_max_amplitude", &digi_max_amplitude, &b_digi_max_amplitude);
+   fChain->SetBranchAddress("digi_pedestal", &digi_pedestal, &b_digi_pedestal);
+   fChain->SetBranchAddress("digi_pedestal_rms", &digi_pedestal_rms, &b_digi_pedestal_rms);
+   fChain->SetBranchAddress("digi_time_at_frac30", &digi_time_at_frac30, &b_digi_time_at_frac30);
+   fChain->SetBranchAddress("digi_time_at_frac50", &digi_time_at_frac50, &b_digi_time_at_frac50);
+   fChain->SetBranchAddress("digi_time_at_max", &digi_time_at_max, &b_digi_time_at_max);
+   Notify();
+}
+
+Bool_t cef3Scan::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void cef3Scan::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t cef3Scan::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef cef3Scan_cxx
