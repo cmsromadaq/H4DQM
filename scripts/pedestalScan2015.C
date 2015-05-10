f645da20c0b50673ca2dbb8b7f040d6fd957c12b
crovelli <crovelli@gmail.com>
2015-05-11 00:20:16 +0200

    update for new hodoscope

---
+++ scripts/pedestalScan2015.C
@@ -0,0 +1,80 @@
+#define pedestalScan2015_cxx
+
+#include <TH1.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <iostream>
+#include <fstream>
+
+
+#include "./pedestalScan2015.h"
+
+using namespace std;
+
+void pedestalScan2015::Loop()
+{
+  if (fChain == 0) return;
+  
+  Long64_t nentries = fChain->GetEntriesFast();  
+  
+  // histos declaration
+  TH1F *H1_Ped_B64[64];
+
+  char histoName[300];
+  for (int ii=0; ii<64; ii++) {
+    sprintf(histoName, "H1_Ped_B64[%d]", ii);
+    H1_Ped_B64[ii] = new TH1F(histoName,histoName,35,735,770);
+  }
+  
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+    for (UInt_t j=0; j<nAdcChannels; j++) {
+      float theAdcValue = adcData[j];
+      int theAdcChannel = adcChannel[j];
+      if (adcBoard[j]==201392129) {
+	if (theAdcChannel>65) cout << "problem?" << endl;
+	if (theAdcChannel>63) continue;   // spare channels
+	H1_Ped_B64[theAdcChannel]->Fill(theAdcValue);
+      }
+    }
+  }
+
+  // Gaussian fits + dump the output into a txt file
+  ofstream *ResFile = new ofstream("../test/pedestalScan.txt",ios::out);
+  *ResFile << "## Board" << "\t\t" << "channel" << "\t\t" << "mean" << "\t\t" << "sigma" << "\t\t" << "chi2" << "\t\t" << "threshold (def: mean+5sigma)" << endl;
+
+  for (int ii=0; ii<64; ii++) {   
+    int peakBin   = H1_Ped_B64[ii]->GetMaximumBin();
+    float h_peak  = H1_Ped_B64[ii]->GetBinCenter(peakBin);
+    float h_rms   = H1_Ped_B64[ii]->GetRMS();
+    H1_Ped_B64[ii]->Fit("gaus");
+    TF1 *fitF = (TF1*)H1_Ped_B64[ii]->GetFunction("gaus");
+    double gausMean  = fitF->GetParameter(1);
+    double gausSigma = fabs(fitF->GetParameter(2));
+    double gausChi2  = 100.;
+    double threshold = gausMean + 5.*gausSigma; 
+    if (fitF->GetNDF()>0) 
+      gausChi2 = fitF->GetChisquare()/fitF->GetNDF() ;
+    if (gausChi2>10){ gausMean = h_peak; gausSigma = h_rms; }
+    *ResFile << "201392129" << "\t" << ii << "\t" << gausMean << "\t" << gausSigma << "\t" << gausChi2 << "\t" << threshold << endl;
+  }
+
+  // Save the histos in plots for the DQM
+  char plotFileName[300];
+  TCanvas c1("c1","",1);
+  c1.Divide(2,2);
+  for (int ii=0; ii<64; ii++) {   
+    int frame = ii%4+1;
+    c1.cd(frame);
+    H1_Ped_B64[ii]->Draw();
+    if (ii%4==3) { 
+      int ratio = ii/4;
+      sprintf(plotFileName, "../test/Board201392129_plots%d.png",ratio);
+      c1.SaveAs(plotFileName);
+    }
+  } 
+}
---
+++ scripts/pedestalScan2015.h
@@ -0,0 +1,225 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Apr 28 11:19:00 2015 by ROOT version 5.34/22
+// from TTree H4tree/H4 testbeam tree
+// found on file: ../bin/1659.root
+//////////////////////////////////////////////////////////
+
+#ifndef pedestalScan2015_h
+#define pedestalScan2015_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class pedestalScan2015 {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   UInt_t          runNumber;
+   UInt_t          spillNumber;
+   UInt_t          evtNumber;
+   UInt_t          evtTimeDist;
+   UInt_t          evtTimeStart;
+   UInt_t          nEvtTimes;
+   ULong64_t       evtTime[1];   //[nEvtTimes]
+   UInt_t          evtTimeBoard[1];   //[nEvtTimes]
+   UInt_t          nAdcChannels;
+   UInt_t          adcBoard[98];   //[nAdcChannels]
+   UInt_t          adcChannel[98];   //[nAdcChannels]
+   UInt_t          adcData[98];   //[nAdcChannels]
+   UInt_t          nTdcChannels;
+   UInt_t          tdcBoard[1];   //[nTdcChannels]
+   UInt_t          tdcChannel[1];   //[nTdcChannels]
+   UInt_t          tdcData[1];   //[nTdcChannels]
+   UInt_t          nDigiSamples;
+   UInt_t          digiFrequency[1];   //[nDigiSamples]
+   UInt_t          digiGroup[1];   //[nDigiSamples]
+   UInt_t          digiChannel[1];   //[nDigiSamples]
+   UInt_t          digiSampleIndex[1];   //[nDigiSamples]
+   Float_t         digiSampleValue[1];   //[nDigiSamples]
+   UInt_t          digiBoard[1];   //[nDigiSamples]
+   UInt_t          nScalerWords;
+   UInt_t          scalerWord[1];   //[nScalerWords]
+   UInt_t          scalerBoard[1];   //[nScalerWords]
+   UInt_t          nPatterns;
+   UInt_t          pattern[1];   //[nPatterns]
+   UInt_t          patternBoard[1];   //[nPatterns]
+   UInt_t          patternChannel[1];   //[nPatterns]
+   UInt_t          nTriggerWords;
+   UInt_t          triggerWords[1];   //[nTriggerWords]
+   UInt_t          triggerWordsBoard[1];   //[nTriggerWords]
+
+   // List of branches
+   TBranch        *b_runNumber;   //!
+   TBranch        *b_spillNumber;   //!
+   TBranch        *b_evtNumber;   //!
+   TBranch        *b_evtTimeDist;   //!
+   TBranch        *b_evtTimeStart;   //!
+   TBranch        *b_nEvtTimes;   //!
+   TBranch        *b_evtTime;   //!
+   TBranch        *b_evtTimeBoard;   //!
+   TBranch        *b_nAdcChannels;   //!
+   TBranch        *b_adcBoard;   //!
+   TBranch        *b_adcChannel;   //!
+   TBranch        *b_adcData;   //!
+   TBranch        *b_nTdcChannels;   //!
+   TBranch        *b_tdcBoard;   //!
+   TBranch        *b_tdcChannel;   //!
+   TBranch        *b_tdcData;   //!
+   TBranch        *b_nDigiSamples;   //!
+   TBranch        *b_digiFrequency;   //!
+   TBranch        *b_digiGroup;   //!
+   TBranch        *b_digiChannel;   //!
+   TBranch        *b_digiSampleIndex;   //!
+   TBranch        *b_digiSampleValue;   //!
+   TBranch        *b_digiBoard;   //!
+   TBranch        *b_nScalerWords;   //!
+   TBranch        *b_scalerWord;   //!
+   TBranch        *b_scalerBoard;   //!
+   TBranch        *b_nPatterns;   //!
+   TBranch        *b_pattern;   //!
+   TBranch        *b_patternBoard;   //!
+   TBranch        *b_patternChannel;   //!
+   TBranch        *b_nTriggerWords;   //!
+   TBranch        *b_triggerWords;   //!
+   TBranch        *b_triggerWordsBoard;   //!
+
+   pedestalScan2015(TTree *tree=0);
+   virtual ~pedestalScan2015();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef pedestalScan2015_cxx
+pedestalScan2015::pedestalScan2015(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("../bin/1659.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("../bin/1659.root");
+      }
+      f->GetObject("H4tree",tree);
+
+   }
+   Init(tree);
+}
+
+pedestalScan2015::~pedestalScan2015()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t pedestalScan2015::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t pedestalScan2015::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void pedestalScan2015::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("runNumber", &runNumber, &b_runNumber);
+   fChain->SetBranchAddress("spillNumber", &spillNumber, &b_spillNumber);
+   fChain->SetBranchAddress("evtNumber", &evtNumber, &b_evtNumber);
+   fChain->SetBranchAddress("evtTimeDist", &evtTimeDist, &b_evtTimeDist);
+   fChain->SetBranchAddress("evtTimeStart", &evtTimeStart, &b_evtTimeStart);
+   fChain->SetBranchAddress("nEvtTimes", &nEvtTimes, &b_nEvtTimes);
+   fChain->SetBranchAddress("evtTime", evtTime, &b_evtTime);
+   fChain->SetBranchAddress("evtTimeBoard", evtTimeBoard, &b_evtTimeBoard);
+   fChain->SetBranchAddress("nAdcChannels", &nAdcChannels, &b_nAdcChannels);
+   fChain->SetBranchAddress("adcBoard", adcBoard, &b_adcBoard);
+   fChain->SetBranchAddress("adcChannel", adcChannel, &b_adcChannel);
+   fChain->SetBranchAddress("adcData", adcData, &b_adcData);
+   fChain->SetBranchAddress("nTdcChannels", &nTdcChannels, &b_nTdcChannels);
+   fChain->SetBranchAddress("tdcBoard", &tdcBoard, &b_tdcBoard);
+   fChain->SetBranchAddress("tdcChannel", &tdcChannel, &b_tdcChannel);
+   fChain->SetBranchAddress("tdcData", &tdcData, &b_tdcData);
+   fChain->SetBranchAddress("nDigiSamples", &nDigiSamples, &b_nDigiSamples);
+   fChain->SetBranchAddress("digiFrequency", &digiFrequency, &b_digiFrequency);
+   fChain->SetBranchAddress("digiGroup", &digiGroup, &b_digiGroup);
+   fChain->SetBranchAddress("digiChannel", &digiChannel, &b_digiChannel);
+   fChain->SetBranchAddress("digiSampleIndex", &digiSampleIndex, &b_digiSampleIndex);
+   fChain->SetBranchAddress("digiSampleValue", &digiSampleValue, &b_digiSampleValue);
+   fChain->SetBranchAddress("digiBoard", &digiBoard, &b_digiBoard);
+   fChain->SetBranchAddress("nScalerWords", &nScalerWords, &b_nScalerWords);
+   fChain->SetBranchAddress("scalerWord", &scalerWord, &b_scalerWord);
+   fChain->SetBranchAddress("scalerBoard", &scalerBoard, &b_scalerBoard);
+   fChain->SetBranchAddress("nPatterns", &nPatterns, &b_nPatterns);
+   fChain->SetBranchAddress("pattern", &pattern, &b_pattern);
+   fChain->SetBranchAddress("patternBoard", &patternBoard, &b_patternBoard);
+   fChain->SetBranchAddress("patternChannel", &patternChannel, &b_patternChannel);
+   fChain->SetBranchAddress("nTriggerWords", &nTriggerWords, &b_nTriggerWords);
+   fChain->SetBranchAddress("triggerWords", triggerWords, &b_triggerWords);
+   fChain->SetBranchAddress("triggerWordsBoard", triggerWordsBoard, &b_triggerWordsBoard);
+   Notify();
+}
+
+Bool_t pedestalScan2015::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void pedestalScan2015::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t pedestalScan2015::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef pedestalScan2015_cxx
