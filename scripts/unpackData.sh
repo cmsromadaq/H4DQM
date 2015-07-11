f2c952cbf8779018b3fbcbd4475bc0e7cbecd7c9
Paolo Meridiani <paolo.meridiani@cern.ch>
2015-07-11 20:26:07 +0200

    Adding EOS capabilities

--- scripts/unpackData.sh
+++ scripts/unpackData.sh
@@ -12,6 +12,7 @@ dryrun=0
 merge=0
 batch=0
 clean=0
+eos=0
 
 
 waitForBatchJobs() {
@@ -72,7 +73,7 @@ mergeSpills()
     done
 }
 
-TEMP=`getopt -o i:o:r:s:q:l:mbdc --long input:,output:,run:,spill:,queue:,log:,merge,batch,dryrun,clean -n 'unpackData.sh' -- "$@"`
+TEMP=`getopt -o i:o:r:s:q:l:mbdce --long input:,output:,run:,spill:,queue:,log:,merge,batch,dryrun,clean,eos -n 'unpackData.sh' -- "$@"`
 if [ $? != 0 ] ; then echo "Options are wrong..." >&2 ; exit 1 ; fi
 
 eval set -- "$TEMP"
@@ -89,26 +90,45 @@ case "$1" in
 -b | --batch ) batch=1; shift;;
 -d | --dryrun ) dryrun=1; shift;;
 -c | --clean ) clean=1; shift;;
+-e | --eos ) eos=1; shift;;
 -- ) shift; break ;;
 * ) break ;;
 esac
 done
 
+eosCommand=""
+[ "$eos" == "1" ] && eosCommand="/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
+
 spillList="$spill"
-[ "$spill" == "-1" ] && spillList=`find ${input}/${run} -regextype 'posix-basic' -regex ".*\.${suffix}" -type f | xargs -I {} basename {} .${suffix} | awk '{printf "%d ",$1}END{printf"\n"}' | sort -n`
+[[ "$spill" == "-1" && "$eos" == "0" ]] && spillList=`find ${input}/${run} -regextype 'posix-basic' -regex ".*\.${suffix}" -type f | xargs -I {} basename {} .${suffix} | awk '{printf "%d ",$1}END{printf"\n"}' | sort -n`
+[[ "$spill" == "-1" && "$eos" == "1" ]] && echo "Checking EOS dir $input"; spillList=`${eosCommand} find -f ${input}/${run} | grep ".${suffix}" | xargs -I {} basename {} .${suffix} | awk '{printf "%d ",$1}END{printf"\n"}' | sort -n`
+
 spillList=( $spillList )
 echo "===>unpackData: input ${input}, run ${run}, spills ${spillList[@]}"
 
 runDir=`echo ${launchDir} | awk -F '/H4DQM' '{printf "%s/H4DQM\n",$1}'`
 
-[ "${dryrun}" == "1" ] ||  mkdir -p ${output}/${run};  mkdir -p ${log}/${run}
+[ "${dryrun}" == "1" ] ||  ${eosCommand} mkdir -p ${output}/${run};  mkdir -p ${log}/${run}
 
 jobList=""
 for spills in ${spillList[@]}; do
-    unpackCommand="${runDir}/bin/unpack -i ${input} -r ${run} -s ${spills} -o ${output}"
-    command=""
+    #prepare job 
+    rm -rf ${log}/${run}/${spills}.sh
+    touch ${log}/${run}/${spills}.sh
+    echo "#!/bin/sh" >>  ${log}/${run}/${spills}.sh
+    echo "cd /cvmfs/cms.cern.ch/slc6_amd64_gcc481/cms/cmssw/CMSSW_7_2_5/src/; eval \`scram runtime -sh\`; cd -" >>  ${log}/${run}/${spills}.sh
+    [ "${eos}" == "1" ] &&  echo "mkdir -p /tmp/${run}; ${eosCommand} cp ${input}/${run}/${spills}.${suffix} /tmp/${run}/${spills}.${suffix}; mkdir -p /tmp/DataTree" >> ${log}/${run}/${spills}.sh
+    jobInputDir=${input}
+    jobOutputDir=${output}
+    [ "${eos}" == "1" ] &&  jobInputDir=/tmp; jobOutputDir=/tmp/DataTree
+    unpackCommand="${runDir}/bin/unpack -i ${jobInputDir} -r ${run} -s ${spills} -o ${jobOutputDir}"
+    echo "${unpackCommand}" >> ${log}/${run}/${spills}.sh
+    [ "${eos}" == "1" ] && echo "${eosCommand} cp ${jobOutputDir}/${run}/${spills}.root ${output}/${run}/${spills}.root"  >> ${log}/${run}/${spills}.sh 
+
+    #launch job
+    command="/bin/bash"
     [ "${batch}" == "1" ] && command="bsub -q ${queue} -o ${log}/${run}/unpack_${spills}.log -e ${log}/${run}/unpack_${spills}.log"
-    command="${command} ${unpackCommand}"
+    command="${command} < ${log}/${run}/${spills}.sh"
     echo ${command}
     [ "${dryrun}" == "1" ] && continue
     jobId=`${command} 2>&1`
