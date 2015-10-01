1373a0cde70cc78e97de818024bd060f081089d3
cmsdaq <cmsroma.daq@gmail.com>
2015-10-01 16:31:19 +0200

    faster dqm

--- scripts/runDQM.sh
+++ scripts/runDQM.sh
@@ -32,7 +32,7 @@ done
 ### /home/cmsdaq/DAQ/H4DQM/bin/plotterTDC -i $output -o $output  -r $run -s $spill 
 #for runtype in beam ped led;do
 if [ $((spill%2)) -eq 1 ]; then
-    if [ $((spill)) -eq 3 ]; then #skip spill 3 so that it's faster to see plots of first spill in the run
+    if [ $((spill)) -ne 3 ]; then #skip spill 3 so that it's faster to see plots of first spill in the run
 	for runtype in ped beam;do
 	    
 	    /home/cmsdaq/DAQ/H4DQM/bin/plotterTotal -i $output -o $output  -r $run -s $spill -t$runtype -I integrated.root 
